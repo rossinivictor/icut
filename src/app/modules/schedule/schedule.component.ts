e97f1ae2b9cfcd79c3125193e2f1a760bda9eebf
Icaro Aoki <aokicaro@hotmail.com>
2018-05-30 18:31:17 -0300

    chamada da agenda.

--- src/app/modules/schedule/schedule.component.ts
+++ src/app/modules/schedule/schedule.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AngularFirestore, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Observable } from '@firebase/util';
+
+import { AuthService } from '../../core/auth.service';
+
+
+
 @Component({
   selector: 'app-schedule',
   templateUrl: './schedule.component.html',
@@ -7,9 +14,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScheduleComponent implements OnInit {
 
-  constructor() { }
+  public estabelecimento: Observable<any>;
+  public uid: string;
+  public schedule: any[];
+
+  constructor(
+    private afs: AngularFirestore,
+    private authService: AuthService
+  ) {
+    this.authService.enterprise.subscribe(
+      (ent) => {
+        // console.log(ent);
+        this.uid = ent.uid;
+        const query = `${'estabelecimento/' + ent.uid + '/agenda'}`;
+        const estabelecimento = this.afs.collection('estabelecimento').doc(ent.uid).collection('agenda').valueChanges();
+        // const estabelecimento[] = this.afs.doc<any>(query).valueChanges();
+        estabelecimento.subscribe(
+          res => console.log(res)
+        );
+        // estabelecimento.subscribe(
+        //   res => {
+        //     console.log(res);
+        //   }
+        // );
+      }
+    );
+
+  }
 
   ngOnInit() {
+
+
   }
 
 }
